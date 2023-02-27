d66b013524b85912bd532e899c1595218a0510bb
Hadjati Darius <darius.hadjati@gmail.com>
2023-02-27 16:36:24 +0100

    setting up server

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 const data = require('./data.json');
-console.log(data);
 
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('pages/index');
+})
+
+app.listen(3000, () => {
+    console.log('ON PORT 3000')
+})
