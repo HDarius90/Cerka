17c575a6d5af90ccce8a477c53987cf791cd7126
Hadjati Darius <darius.hadjati@gmail.com>
2023-03-03 21:09:44 +0100

    created funcions.ejs to be able to pass functions to a template

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const app = express();
 const path = require('path');
 const data = require('./data.json');
 const methodOverride = require('method-override')
+const { v4: uuid } = require('uuid');
+
+//creating uniq id-s for referals
+for (let student of data.class) {
+    for (let ref of student.officeReferals) {
+        ref.id = uuid();
+    }
+}
+
 
 
 
@@ -24,8 +33,8 @@ app.get('/grades', (req, res) => {
 })
 app.get('/grade/:name', (req, res) => {
     const { name } = req.params;
-    const student = data.class.find( st => st.name === name);
-    
+    const student = data.class.find(st => st.name === name);
+
     res.render('pages/show', { student });
 })
 
@@ -35,14 +44,14 @@ app.get('/newgrade', (req, res) => {
 
 app.post('/grade/newgrade', (req, res) => {
     const { name, subject, grade } = req.body;
-    const student = data.class.find( st => st.name === name);
+    const student = data.class.find(st => st.name === name);
     try {
-        student.grades[subject].push(grade);        
+        student.grades[subject].push(parseInt(grade));
     } catch (error) {
         res.redirect('/grades');
         console.log(error);
     }
-    res.redirect('/grades');
+    res.redirect('/');
 })
 
 app.get('/reqref', (req, res) => {
@@ -50,13 +59,19 @@ app.get('/reqref', (req, res) => {
 })
 
 app.get('/referals', (req, res) => {
-    const {name} = req.query;
-    const student = data.class.find( st => st.name === name);
-    if(!student){
+    const { name } = req.query;
+    const student = data.class.find(st => st.name === name);
+    if (!student) {
         res.redirect('/reqref');
     }
-    res.render('pages/referals', {student})
+    res.render('pages/referals', { student })
 
+})
+
+app.patch('/referals', (req, res) => {
+    const { checkbox } = req.body;
+    console.log(checkbox);
+    res.send("csá")
 })
 
 app.listen(3000, () => {
